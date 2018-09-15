d85ddb4c9125f89cc28bb09534b249f097305638
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2018-09-15 22:55:02 +0200

    First draft of the fetching mechanism

--- api/controllers/UniController.js
+++ api/controllers/UniController.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var ResourceHelper = require('../helper/ResourceHelper');
+
 var DAYS_ACCEPTED = ['mo', 'di', 'mi', 'do', 'fr', 'sa', 'so'];
 
 module.exports.getForDay = function () {
@@ -11,5 +13,7 @@ module.exports.getForDay = function () {
         } else {
             res.status(400).send('Specified day is invalid. Accepted are: ' + JSON.stringify(DAYS_ACCEPTED));
         }
+
+        ResourceHelper.fetchMenu(ResourceHelper.LOCATION_TAGS[0], ResourceHelper.getCurrentWeek());
     };
 };
