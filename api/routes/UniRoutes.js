580fbaa8c392b5af6d8608dd93eac293599d8862
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2018-09-16 22:20:23 +0200

    Implement caching for menus more mensas

--- api/routes/UniRoutes.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-module.exports = function (app) {
-    var UniController = require('../controllers/UniController');
-
-    app.route('/mensa/uni/:day')
-        .get(UniController.getForDay());
-};
