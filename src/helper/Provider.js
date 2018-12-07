3f84396b59871b7fb7b56cdd60c37257e3d28e08
Lukas Wolfsteiner <lukas@wolfsteiner.media>
2018-12-07 21:38:25 +0100

    Add static day validation with day aliases

--- src/helper/Provider.js
+++ src/helper/Provider.js
@@ -5,7 +5,7 @@ const LOCATIONS_ALIASES = {
   'oth': ['othr', 'oth-regensburg'],
   'oth-abend': ['oth-evening'],
   'pruefening': ['oth-pruefening', 'oth-prüfening', 'prüfening'],
-}
+};
 const LOCATIONS = Object.keys(LOCATIONS_ALIASES);
 
 function isValidLocation(locationValue) {
@@ -21,3 +21,27 @@ function isValidLocation(locationValue) {
   return found;
 }
 module.exports.isValidLocation = isValidLocation;
+
+const DAYS_ALIASES = {
+  'mo': ['mo', 'montag', 'monday'],
+  'di': ['di', 'dienstag', 'tuesday'],
+  'mi': ['mi', 'mittwoch', 'wednesday'],
+  'do': ['do', 'donnerstag', 'thursday'],
+  'fr': ['fr', 'freitag', 'friday'],
+  'sa': ['sa', 'samstag', 'saturday'],
+  'so': ['so', 'sonntag', 'sunday']
+};
+const DAYS = Object.keys(DAYS_ALIASES);
+
+function isValidDay(dayValue) {
+  let found = false;
+
+  DAYS.forEach(dayKey => {
+    if (dayKey === dayValue || DAYS_ALIASES[dayKey].indexOf(dayValue) > -1) {
+      found = true;
+    }
+  });
+
+  return found;
+}
+module.exports.isValidDay = isValidDay;
